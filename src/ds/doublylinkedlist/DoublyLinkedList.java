5ac6d42d3a6cfc1d544aec1ef7225750f20455c9
Hong Yi Meng <menghongyi@gmail.com>
2019-03-19 15:29:07 -0700

    doubly linked list 2

--- src/ds/doublylinkedlist/DoublyLinkedList.java
+++ src/ds/doublylinkedlist/DoublyLinkedList.java
@@ -1,5 +1,104 @@
 package ds.doublylinkedlist;
 
 public class DoublyLinkedList {
-
+	private Node first;
+	private Node last;
+	
+	public DoublyLinkedList() {
+		this.first = null;
+		this.last = null;
+	}
+	
+	public boolean isEmpty() {
+		return first == null;
+	}
+	
+	public void insertFirst(int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if(isEmpty()) {
+			last = newNode; //if empty, last will refer to the new Node bein created
+		} else {
+			first.previous = newNode;
+		}
+		newNode.next = first; // the new node's next field will point to the old first
+		this.first = newNode;
+	}
+	
+	public void insertLast(int data) {
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if(isEmpty()) {
+			first = newNode;
+		} else {
+			last.next = newNode; // assigning old last to newNode
+			newNode.previous = last; // the old last will be the newNode's previous
+		}
+		last = newNode; //the linkedList's last field should point to the new node
+	}
+	
+	// assume non-empty
+	public Node deleteFirst() {
+		Node temp = first;
+		if(first.next == null) { // there is only one item in the list
+			last = null;
+		} else {
+			first.next.previous = null; // the list's first node will point to null
+		}
+		
+		first = first.next; // we are assigning the reference of the node following the old first node 
+		                    	//to the first field of the linked list object
+		return temp; // return the deleted old first node
+	}
+	
+	// assume non-empty list
+	public Node deleteLast() {
+		Node temp = last;
+		if(first.next == null) { // we only have one node in this list
+			first = null;
+		} else {
+			last.previous.next = null; // the last node's previous node's next field will point to null	
+		}
+		last = last.previous;
+		return temp;
+	}
+	
+	// assume a non-empty list
+	public boolean insertAfter(int key, int data) {
+		Node current = first; // we start from the beginning of the list
+		while(current.data != key) { // as long as we have not found the key in a particular node
+			current = current.next;
+			if(current == null) {
+				return false;
+			}
+		}
+		
+		Node newNode = new Node();
+		newNode.data = data;
+		
+		if(current == last) {
+			current.next = null;
+			last = newNode;
+		} else {
+			newNode.next = current.next; // new node's next field should point to the node ahead of 
+										 	//the current node's next field
+			current.next.previous = newNode; // the node ahead of current, it's previous field should point 
+											 	//to newNode
+		}
+		
+		newNode.previous = current;
+		current.next = newNode;
+		
+		return true;
+	}
+	
+	// assume non-empty list
+	public Node deleteKey(int key) {
+		
+	}
+	
+	
+	
 }
